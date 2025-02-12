4ea6bbe7bb6faec400366ee303cff370adac423c
Biswas Achaju <biswasachaju@yahoo.com>
2025-02-12 07:40:15 +0545

    Fixed API route issue

--- src/app/api/clients/edit/route.js
+++ src/app/api/clients/edit/route.js
@@ -1,27 +1,26 @@
 import { NextResponse } from "next/server";
-
 import db from "@/lib/db";
 
 export async function PATCH(request) {
     try {
         const body = await request.json();
-        const {
-            id,
-            title,
-            logo,
-            categoryId,
-        } = body;
+        const { id, title, logo, categoryId } = body;
 
-        const Clients = await db.Clients.update({
+        // Validate input
+        if (!id) {
+            return NextResponse.json({ error: "Client ID is required" }, { status: 400 });
+        }
+
+        const updatedClient = await db.client.update({
             where: { id },
             data: {
                 title,
                 logo,
-                categoryId: Number(categoryId) ,
+                categoryId: categoryId ? Number(categoryId) : null, // Convert to number
             },
         });
 
-        return NextResponse.json(Clients, { status: 200 });
+        return NextResponse.json(updatedClient, { status: 200 });
     } catch (error) {
         console.error("Error updating Clients:", error);
         return NextResponse.json({ error: "Error updating Clients" }, { status: 500 });
